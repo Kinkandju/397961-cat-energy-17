252c028738982acbbcde845719296b33e47237a9
Kinkandju <ferretsphilosophy@gmail.com>
2019-08-21 19:29:42 +0300

    Подключение скрипта меню мобильной версии

---
+++ source/js/script.js
@@ -0,0 +1,18 @@
+var navToggle = document.querySelector('.page-header__toggle');
+var navMain = document.querySelector('.main-nav');
+
+    navToggle.addEventListener('click', function() {
+      if (navToggle.classList.contains('page-header__toggle--opened')) {
+        navToggle.classList.remove('page-header__toggle--opened');
+        navToggle.classList.add('page-header__toggle--closed');
+        navMain.classList.remove('main-nav--opened');
+        navMain.classList.add('main-nav--closed');
+      } else {
+        navToggle.classList.remove('page-header__toggle--closed');
+        navToggle.classList.add('page-header__toggle--opened');
+        navMain.classList.remove('main-nav--closed');
+        navMain.classList.add('main-nav--opened');
+      }
+    });
+    
+ 
