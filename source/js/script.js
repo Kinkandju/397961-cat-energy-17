926cbed88ddda2ee6f1dc4a8f3e110a2af527d8e
Kinkandju <ferretsphilosophy@gmail.com>
2019-09-01 16:31:02 +0300

    Форма ввода

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,4 @@
+// Меню в мобильной версии
 var navToggle = document.querySelector('.page-header__toggle');
 var navMain = document.querySelector('.main-nav');
 
@@ -10,9 +11,28 @@ var navMain = document.querySelector('.main-nav');
     } else {
       navToggle.classList.remove('page-header__toggle--closed');
       navToggle.classList.add('page-header__toggle--opened');
-  navMain.classList.remove('main-nav--opened');
+      navMain.classList.remove('main-nav--opened');
       navMain.classList.add('main-nav--closed');
     }
   });
-    
- 
+
+// Поля ввода формы
+  var form = document.querySelector('.form-main__form');
+  var fields = form.querySelectorAll('.form__item input:required');
+  var submitButton = form.querySelector('button');
+
+  form.addEventListener('input', function() {
+    for (var i = 0; i < fields.length; i++) {
+      if (fields[i].validity.valid) {
+        fields[i].parentElement.classList.remove('error');
+      }
+    }
+  });
+
+  submitButton.addEventListener('click', function() {
+    for (var i = 0; i < fields.length; i++) {
+      if (!fields[i].validity.valid) {
+        fields[i].parentElement.classList.add('error');
+      }
+    }
+  });
