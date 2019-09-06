20a7de7a6412b3edad5d95c1e8f29bc3017a17a0
Kinkandju <ferretsphilosophy@gmail.com>
2019-09-06 22:00:16 +0300

    Исправление критериев: Б3, Б8, Б11, Б17, Б30, Б31, Д22

--- source/js/script.js
+++ source/js/script.js
@@ -3,6 +3,7 @@ var navToggle = document.querySelector('.page-header__toggle');
 var navMain = document.querySelector('.main-nav');
 
   navMain.classList.remove('main-nav--nojs');
+  navToggle.classList.remove('page-header__toggle--nojs');
 
   navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
